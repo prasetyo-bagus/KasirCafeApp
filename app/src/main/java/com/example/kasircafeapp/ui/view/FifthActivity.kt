f44dc73302f88f13a8eed7e344036b33e209aba1
Prasetyo Bagus <bagusprasetyo.work@gmail.com>
2024-10-17 19:42:53 +0700

    Create Simple Dashboard Kasir

--- app/src/main/java/com/example/kasircafeapp/ui/view/FifthActivity.kt
+++ app/src/main/java/com/example/kasircafeapp/ui/view/FifthActivity.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.kasircafeapp.R
 
-class SecondActivity : AppCompatActivity() {
+class FifthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_second)
+        setContentView(R.layout.activity_fifth)
 
     }
 }
--- app/src/main/java/com/example/kasircafeapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/kasircafeapp/ui/view/MainActivity.kt
@@ -1,21 +1,43 @@
 package com.example.kasircafeapp.ui.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.kasircafeapp.R
+import com.example.kasircafeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonTransaksi.setOnClickListener{
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
+
+        binding.buttonMakanan.setOnClickListener{
+            val intent = Intent(this, ThirdActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.buttonMinuman.setOnClickListener{
+            val intent = Intent(this, FourthActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.buttonAdmin.setOnClickListener{
+            val intent = Intent(this, FifthActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
